89d48f521bc18c4b2f68ed97e95214b00faa58f0
juafercito <jonatitojalm@gmail.com>
2025-04-08 22:56:11 -0500

    fixing vite.config for enhancing perform rate for SEO

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import removeConsole from 'vite-plugin-remove-console';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), removeConsole()],
   server: {
     port: 4000,
   },
@@ -14,8 +15,11 @@ export default defineConfig({
       output: {
         manualChunks: (id) => {
           if (id.includes('node_modules')) return 'vendor';
-          if (id.includes('UserBurguerMenu')) return 'user-menu';
+          if (id.includes('node_modules/react')) return 'vendor-react';
+          if (id.includes('node_modules/react-router-dom')) return 'vendor-react-router-dom';
+          if (id.includes('styled-components')) return 'vendor-styled-components';
 
+          if (id.includes('UserBurguerMenu')) return 'user-menu';
           if (id.includes('Dashboard')) return 'dashboard';
           if (id.includes('AdminPanel')) return 'admin';
         },
